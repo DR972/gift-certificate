b2a6821e9f77f8ab5680367210f9497eb5d98952
Dzmitry Rozmysl <dzmitry_rozmysl@epam.com>
2022-04-16 09:36:14 +0300

    update exception classes

--- service/src/main/java/com/epam/esm/service/exception/DuplicateEntityException.java
+++ service/src/main/java/com/epam/esm/service/exception/DuplicateEntityException.java
@@ -1,18 +1,7 @@
 package com.epam.esm.service.exception;
 
 public class DuplicateEntityException extends RuntimeException {
-    public DuplicateEntityException() {
-    }
-
     public DuplicateEntityException(String message) {
         super(message);
-    }
-
-    public DuplicateEntityException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public DuplicateEntityException(Throwable cause) {
-        super(cause);
     }
 }
--- service/src/main/java/com/epam/esm/service/exception/NoSuchEntityException.java
+++ service/src/main/java/com/epam/esm/service/exception/NoSuchEntityException.java
@@ -1,18 +1,7 @@
 package com.epam.esm.service.exception;
 
 public class NoSuchEntityException extends RuntimeException {
-    public NoSuchEntityException() {
-    }
-
     public NoSuchEntityException(String message) {
         super(message);
-    }
-
-    public NoSuchEntityException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public NoSuchEntityException(Throwable cause) {
-        super(cause);
     }
 }
