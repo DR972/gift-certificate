df45eb0db66596f8903777eaeaf5e0c8d06322f0
Dzmitry Rozmysl <dzmitry_rozmysl@epam.com>
2022-04-16 16:18:29 +0300

    update SpringConfig.class

--- web/src/main/java/com/epam/esm/config/SpringConfig.java
+++ web/src/main/java/com/epam/esm/config/SpringConfig.java
@@ -3,14 +3,8 @@ package com.epam.esm.config;
 import org.springframework.context.annotation.*;
 import org.springframework.web.servlet.config.annotation.*;
 
-import java.time.Clock;
-
 @Configuration
 @ComponentScan("com.epam.esm")
 @EnableWebMvc
 public class SpringConfig implements WebMvcConfigurer {
-    @Bean
-    public Clock clock() {
-        return Clock.systemDefaultZone();
-    }
 }
