5906477a4d4da3a9f2a0372ce2d43b11ff910e41
Dzmitry Rozmysl <dzmitry_rozmysl@epam.com>
2022-04-16 09:49:23 +0300

    update Entity.class

--- model/src/main/java/com/epam/esm/entity/Entity.java
+++ model/src/main/java/com/epam/esm/entity/Entity.java
@@ -10,14 +10,5 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 public abstract class Entity<ID> implements Serializable {
-
     protected ID id;
-
-    public ID getId() {
-        return id;
-    }
-
-    public void setId(ID id) {
-        this.id = id;
-    }
 }
