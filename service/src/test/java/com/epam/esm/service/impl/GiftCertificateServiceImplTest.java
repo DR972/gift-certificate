30c76a078b46195b3fb7ec7e74f290001c5057fc
Dzmitry Rozmysl <dzmitry_rozmysl@epam.com>
2022-04-17 12:50:43 +0300

    update ServiceTests

--- service/src/test/java/com/epam/esm/service/impl/GiftCertificateServiceImplTest.java
+++ service/src/test/java/com/epam/esm/service/impl/GiftCertificateServiceImplTest.java
@@ -6,9 +6,11 @@ import com.epam.esm.dao.TagDao;
 import com.epam.esm.entity.GiftCertificate;
 import com.epam.esm.entity.Tag;
 import com.epam.esm.service.DateHandler;
+import com.epam.esm.service.exception.NoSuchEntityException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -20,16 +22,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.anyString;
-import static org.mockito.Mockito.any;
-import static com.epam.esm.service.util.ColumnName.LAST_UPDATE_DATE;
-import static com.epam.esm.service.util.ColumnName.DESC;
-import static com.epam.esm.service.util.ColumnName.NAME;
 import static com.epam.esm.service.util.SqlQueryTest.FIND_CERTIFICATE_BY_ID;
 import static com.epam.esm.service.util.SqlQueryTest.FIND_ALL_CERTIFICATES_ORDER_BY_DATE_DESC;
 import static com.epam.esm.service.util.SqlQueryTest.VISIT;
@@ -42,25 +34,24 @@ import static com.epam.esm.service.util.SqlQueryTest.CREATE_CERTIFICATE;
 import static com.epam.esm.service.util.SqlQueryTest.UPDATE_CERTIFICATE_FIELDS_DESCRIPTION_DURATION;
 import static com.epam.esm.service.util.SqlQueryTest.DELETE_CERTIFICATE_TAG_BY_CERTIFICATE_ID;
 import static com.epam.esm.service.util.SqlQueryTest.DELETE_CERTIFICATE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.any;
+import static com.epam.esm.service.util.ColumnName.LAST_UPDATE_DATE;
+import static com.epam.esm.service.util.ColumnName.DESC;
+import static com.epam.esm.service.util.ColumnName.NAME;
 
 @ExtendWith(MockitoExtension.class)
 class GiftCertificateServiceImplTest {
     private static final String TEXT = "text";
     private static final String TAG = "tag";
     private static final String SORTING = "sorting";
-
-    @Mock
-    private GiftCertificateDao certificateDao = mock(GiftCertificateDao.class);
-    @Mock
-    private TagDao tagDao = mock(TagDao.class);
-    @Mock
-    private GiftCertificateTagDao certificateTagDao = mock(GiftCertificateTagDao.class);
-    @Mock
-    private DateHandler dateHandler = Mockito.mock(DateHandler.class);
-
-    @InjectMocks
-    private GiftCertificateServiceImpl certificateServiceImpl;
-
     private static final LocalDateTime DATE_TIME = LocalDateTime.parse("2022-05-01T00:00:00.001");
     private static final GiftCertificate GIFT_CERTIFICATE_1 = new GiftCertificate(1, "ATV riding",
             "Description ATV riding", new BigDecimal("100"), 10, LocalDateTime.parse("2022-04-01T10:12:45.123"),
@@ -97,35 +88,54 @@ class GiftCertificateServiceImplTest {
             "Description shopping at the tool store", null, 10, null,
             null, Arrays.asList(new Tag("shopping"), new Tag("tool"), new Tag("new")));
 
+    @Mock
+    private GiftCertificateDao certificateDao = mock(GiftCertificateDao.class);
+    @Mock
+    private TagDao tagDao = mock(TagDao.class);
+    @Mock
+    private GiftCertificateTagDao certificateTagDao = mock(GiftCertificateTagDao.class);
+    @Mock
+    private DateHandler dateHandler = mock(DateHandler.class);
+    @InjectMocks
+    private GiftCertificateServiceImpl certificateServiceImpl;
+
     @Test
-    void findCertificate() {
+    void findCertificateShouldReturnResult() {
         when(certificateDao.findEntity(FIND_CERTIFICATE_BY_ID, 2L)).thenReturn(Optional.of(GIFT_CERTIFICATE_2));
         certificateServiceImpl.findCertificate(2);
         verify(certificateDao, times(1)).findEntity(FIND_CERTIFICATE_BY_ID, 2L);
+        assertEquals(GIFT_CERTIFICATE_2, certificateServiceImpl.findCertificate(2));
     }
 
     @Test
-    void findListCertificates() {
-        findListCertificatesTest(Arrays.asList(null, null, Collections.singletonList(LAST_UPDATE_DATE + DESC)),
+    void findCertificateShouldThrowException() {
+        when(certificateDao.findEntity(FIND_CERTIFICATE_BY_ID, 2L)).thenReturn(Optional.empty());
+        Exception exception = assertThrows(NoSuchEntityException.class, () -> certificateServiceImpl.findCertificate(2));
+        assertTrue(exception.getMessage().contains("There is no Gift Certificate with this ID = " + 2));
+    }
+
+    @Test
+    void findListCertificatesShouldReturnResult() {
+        testFindListCertificatesShouldReturnResult(Arrays.asList(null, null, Collections.singletonList(LAST_UPDATE_DATE + DESC)),
                 Arrays.asList(GIFT_CERTIFICATE_6, GIFT_CERTIFICATE_4, GIFT_CERTIFICATE_1, GIFT_CERTIFICATE_2, GIFT_CERTIFICATE_3, GIFT_CERTIFICATE_5, GIFT_CERTIFICATE_7),
                 FIND_ALL_CERTIFICATES_ORDER_BY_DATE_DESC, 5, 0, 5);
 
-        findListCertificatesTest(Arrays.asList(Collections.singletonList(VISIT), null, Collections.singletonList(LAST_UPDATE_DATE + DESC)),
+        testFindListCertificatesShouldReturnResult(Arrays.asList(Collections.singletonList(VISIT), null, Collections.singletonList(LAST_UPDATE_DATE + DESC)),
                 Arrays.asList(GIFT_CERTIFICATE_4, GIFT_CERTIFICATE_3),
                 FIND_ALL_CERTIFICATES_BY_PART_NAME_OR_DESCRIPTION_ORDER_BY_DATE_DESC, VISIT, VISIT, 5, 0, 5);
 
-        findListCertificatesTest(Arrays.asList(null, Collections.singletonList(REST), Collections.singletonList(LAST_UPDATE_DATE + DESC)),
+        testFindListCertificatesShouldReturnResult(Arrays.asList(null, Collections.singletonList(REST), Collections.singletonList(LAST_UPDATE_DATE + DESC)),
                 Arrays.asList(GIFT_CERTIFICATE_1, GIFT_CERTIFICATE_2, GIFT_CERTIFICATE_7),
                 FIND_ALL_CERTIFICATES_WITH_SPECIFIC_TAG_ORDER_BY_DATE_DESC, REST, 5, 0, 5);
 
-        findListCertificatesTest(Arrays.asList(Collections.singletonList(RIDING), Collections.singletonList(REST),
+        testFindListCertificatesShouldReturnResult(Arrays.asList(Collections.singletonList(RIDING), Collections.singletonList(REST),
                         Arrays.asList(LAST_UPDATE_DATE + DESC, NAME)),
                 Arrays.asList(GIFT_CERTIFICATE_1, GIFT_CERTIFICATE_2),
                 FIND_ALL_CERTIFICATES_BY_PART_NAME_OR_DESCRIPTION_WITH_SPECIFIC_TAG_ORDER_BY_DATE_AND_NAME,
                 RIDING, RIDING, REST, 5, 0, 5);
     }
 
-    private void findListCertificatesTest(List<List<String>> list, List<GiftCertificate> certificates, String query, Object... queryParams) {
+    private void testFindListCertificatesShouldReturnResult(List<List<String>> list, List<GiftCertificate> certificates, String query, Object... queryParams) {
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         if (list.get(0) != null) {
             params.put(TEXT, list.get(0));
@@ -141,7 +151,7 @@ class GiftCertificateServiceImplTest {
     }
 
     @Test
-    void createCertificate() {
+    void createCertificateTest() {
         when(dateHandler.getCurrentDate()).thenReturn(DATE_TIME);
         when(certificateDao.findEntity(FIND_CERTIFICATE_BY_ID, 0L)).thenReturn(Optional.of(GIFT_CERTIFICATE_8));
         certificateServiceImpl.createCertificate(GIFT_CERTIFICATE_8);
@@ -156,10 +166,11 @@ class GiftCertificateServiceImplTest {
 
         verify(tagDao, times(1)).updateEntity(anyString(), any());
         verify(certificateTagDao, times(1)).updateEntity(anyString(), any());
+        assertEquals(GIFT_CERTIFICATE_8, certificateServiceImpl.createCertificate(GIFT_CERTIFICATE_8));
     }
 
     @Test
-    void updateCertificate() {
+    void updateCertificateTest() {
         when(dateHandler.getCurrentDate()).thenReturn(DATE_TIME);
         when(certificateDao.findEntity(FIND_CERTIFICATE_BY_ID, 5L)).thenReturn(Optional.of(GIFT_CERTIFICATE_9));
         certificateServiceImpl.updateCertificate(GIFT_CERTIFICATE_9, 5);
@@ -169,13 +180,28 @@ class GiftCertificateServiceImplTest {
 
         verify(tagDao, times(1)).updateEntity(anyString(), any());
         verify(certificateTagDao, times(2)).updateEntity(anyString(), any());
+        assertEquals(GIFT_CERTIFICATE_9, certificateServiceImpl.updateCertificate(GIFT_CERTIFICATE_9, 5));
     }
 
     @Test
-    void deleteCertificate() {
+    void updateCertificateShouldThrowException() {
+        when(certificateDao.findEntity(FIND_CERTIFICATE_BY_ID, 2L)).thenReturn(Optional.empty());
+        Exception exception = assertThrows(NoSuchEntityException.class, () -> certificateServiceImpl.updateCertificate(GIFT_CERTIFICATE_5, 2));
+        assertTrue(exception.getMessage().contains("There is no Gift Certificate with this ID = " + 2));
+    }
+
+    @Test
+    void deleteCertificateTest() {
         when(certificateDao.findEntity(FIND_CERTIFICATE_BY_ID, 3L)).thenReturn(Optional.of(GIFT_CERTIFICATE_3));
         certificateServiceImpl.deleteCertificate(3);
         verify(certificateTagDao, times(1)).updateEntity(DELETE_CERTIFICATE_TAG_BY_CERTIFICATE_ID, 3L);
         verify(certificateDao, times(1)).updateEntity(DELETE_CERTIFICATE, 3L);
+    }
+
+    @Test
+    void deleteCertificateShouldThrowException() {
+        when(certificateDao.findEntity(FIND_CERTIFICATE_BY_ID, 2L)).thenReturn(Optional.empty());
+        Exception exception = assertThrows(NoSuchEntityException.class, () -> certificateServiceImpl.deleteCertificate(2));
+        assertTrue(exception.getMessage().contains("There is no Gift Certificate with this ID = " + 2));
     }
 }
--- service/src/test/java/com/epam/esm/service/impl/TagServiceImplTest.java
+++ service/src/test/java/com/epam/esm/service/impl/TagServiceImplTest.java
@@ -3,12 +3,15 @@ package com.epam.esm.service.impl;
 import com.epam.esm.dao.GiftCertificateTagDao;
 import com.epam.esm.dao.TagDao;
 import com.epam.esm.entity.Tag;
+import com.epam.esm.service.exception.DuplicateEntityException;
+import com.epam.esm.service.exception.NoSuchEntityException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import static com.epam.esm.service.util.SqlQueryTest.FIND_TAG_BY_ID;
@@ -20,6 +23,9 @@ import static com.epam.esm.service.util.SqlQueryTest.CREATE_TAG;
 import static com.epam.esm.service.util.SqlQueryTest.UPDATE_TAG;
 import static com.epam.esm.service.util.SqlQueryTest.DELETE_CERTIFICATE_TAG_BY_TAG_ID;
 import static com.epam.esm.service.util.SqlQueryTest.DELETE_TAG;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
@@ -27,60 +33,101 @@ import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 public class TagServiceImplTest {
-    @Mock
-    private TagDao tagDao = mock(TagDao.class);
-    @Mock
-    private GiftCertificateTagDao certificateTagDao = mock(GiftCertificateTagDao.class);
-
-    @InjectMocks
-    private TagServiceImpl tagService;
-
     private static final Tag TAG_1 = new Tag(12, "rest");
     private static final Tag TAG_2 = new Tag(2, "nature");
     private static final Tag TAG_3 = new Tag(3, "shopping");
     private static final Tag NEW_TAG = new Tag(0, NEW);
 
+    @Mock
+    private TagDao tagDao = mock(TagDao.class);
+    @Mock
+    private GiftCertificateTagDao certificateTagDao = mock(GiftCertificateTagDao.class);
+    @InjectMocks
+    private TagServiceImpl tagService;
+
     @Test
-    void findTagById() {
+    void findTagByIdShouldReturnResult() {
         when(tagDao.findEntity(FIND_TAG_BY_ID, 2L)).thenReturn(Optional.of(TAG_2));
-        tagService.findTagById(2L);
+        tagService.findTagById(2);
         verify(tagDao, times(1)).findEntity(FIND_TAG_BY_ID, 2L);
+        assertEquals(TAG_2, tagService.findTagById(2));
     }
 
     @Test
-    void findTagByName() {
+    void findTagByIdShouldThrowException() {
+        when(tagDao.findEntity(FIND_TAG_BY_ID, 2L)).thenReturn(Optional.empty());
+        Exception exception = assertThrows(NoSuchEntityException.class, () -> tagService.findTagById(2));
+        assertTrue(exception.getMessage().contains("There is no tag with this ID = " + 2));
+    }
+
+    @Test
+    void findTagByNameShouldReturnResult() {
         when(tagDao.findEntity(FIND_TAG_BY_NAME, REST)).thenReturn(Optional.of(TAG_1));
         tagService.findTagByName(REST);
         verify(tagDao, times(1)).findEntity(FIND_TAG_BY_NAME, REST);
+        assertEquals(TAG_1, tagService.findTagByName(REST));
     }
 
     @Test
-    void findAllTags() {
+    void findAllTagsShouldReturnResult() {
+        when(tagDao.findListEntities(FIND_ALL_TAGS)).thenReturn(Arrays.asList(TAG_1, TAG_2, TAG_3));
         tagService.findAllTags();
         verify(tagDao, times(1)).findListEntities(FIND_ALL_TAGS);
+        assertEquals(Arrays.asList(TAG_1, TAG_2, TAG_3), tagService.findAllTags());
     }
 
     @Test
-    void createTag() {
+    void createTagTest() {
         when(tagDao.findEntity(FIND_TAG_BY_NAME, NEW)).thenReturn(Optional.of(new Tag()));
         when(tagDao.findEntity(FIND_TAG_BY_ID, 0L)).thenReturn(Optional.of(NEW_TAG));
         tagService.createTag(NEW_TAG);
         verify(tagDao, times(1)).createEntity(CREATE_TAG, NEW);
+        assertEquals(NEW_TAG, tagService.createTag(NEW_TAG));
     }
 
     @Test
-    void updateTag() {
+    void createTagShouldThrowException() {
+        when(tagDao.findEntity(FIND_TAG_BY_NAME, REST)).thenReturn(Optional.of(TAG_1));
+        Exception exception = assertThrows(DuplicateEntityException.class, () -> tagService.createTag(TAG_1));
+        assertTrue(exception.getMessage().contains("A tag named '" + REST + "' already exists"));
+    }
+
+    @Test
+    void updateTagTest() {
         when(tagDao.findEntity(FIND_TAG_BY_NAME, NEW)).thenReturn(Optional.of(new Tag()));
         when(tagDao.findEntity(FIND_TAG_BY_ID, 3L)).thenReturn(Optional.of(TAG_3));
         tagService.updateTag(NEW_TAG, 3L);
         verify(tagDao, times(1)).updateEntity(UPDATE_TAG, NEW, 3L);
+        assertEquals(TAG_3, tagService.updateTag(NEW_TAG, 3L));
     }
 
     @Test
-    void deleteTag() {
+    void updateTagShouldThrowDuplicateEntityException() {
+        when(tagDao.findEntity(FIND_TAG_BY_NAME, REST)).thenReturn(Optional.of(TAG_1));
+        Exception exception = assertThrows(DuplicateEntityException.class, () -> tagService.updateTag(TAG_1, TAG_2.getId()));
+        assertTrue(exception.getMessage().contains("A tag named '" + REST + "' already exists"));
+    }
+
+    @Test
+    void updateTagShouldThrowNoSuchEntityException() {
+        when(tagDao.findEntity(FIND_TAG_BY_NAME, TAG_1.getName())).thenReturn(Optional.of(new Tag()));
+        when(tagDao.findEntity(FIND_TAG_BY_ID, 2L)).thenReturn(Optional.empty());
+        Exception exception = assertThrows(NoSuchEntityException.class, () -> tagService.updateTag(TAG_1, TAG_2.getId()));
+        assertTrue(exception.getMessage().contains("There is no tag with this ID = " + 2));
+    }
+
+    @Test
+    void deleteTagTest() {
         when(tagDao.findEntity(FIND_TAG_BY_ID, 3L)).thenReturn(Optional.of(TAG_3));
         tagService.deleteTag(3);
         verify(certificateTagDao, times(1)).updateEntity(DELETE_CERTIFICATE_TAG_BY_TAG_ID, 3L);
         verify(tagDao, times(1)).updateEntity(DELETE_TAG, 3L);
+    }
+
+    @Test
+    void deleteTagShouldThrowNoSuchEntityException() {
+        when(tagDao.findEntity(FIND_TAG_BY_ID, 2L)).thenReturn(Optional.empty());
+        Exception exception = assertThrows(NoSuchEntityException.class, () -> tagService.deleteTag(TAG_2.getId()));
+        assertTrue(exception.getMessage().contains("There is no tag with this ID = " + 2));
     }
 }
